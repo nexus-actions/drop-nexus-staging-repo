fe83783967a063540320ace3c8942608246705a1
Romain Boisselle <romain@boisselle.dev>
2021-02-26 16:04:11 +0100

    renaimg: `staging` for everyone

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ response=$(curl -s --request POST -u "$INPUT_USERNAME:$INPUT_PASSWORD" \
   --url ${INPUT_BASE_URL}staging/bulk/drop \
   --header 'Accept: application/json' \
   --header 'Content-Type: application/json' \
-  --data '{ "data" : {"stagedRepositoryIds":["'"$INPUT_STAGED_REPOSITORY_ID"'"], "description":"Drop '"$INPUT_STAGED_REPOSITORY_ID"'." } }')
+  --data '{ "data" : {"stagedRepositoryIds":["'"$INPUT_STAGING_REPOSITORY_ID"'"], "description":"Drop '"$INPUT_STAGING_REPOSITORY_ID"'." } }')
 
 if [ ! -z "$response" ]; then
-    echo "Error while dropping staged repository $INPUT_STAGED_REPOSITORY_ID : $response."
+    echo "Error while dropping staged repository $INPUT_STAGING_REPOSITORY_ID : $response."
     exit 1
 fi
