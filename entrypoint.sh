ea6c36873722dc152f496943ab912b3eff158536
Romain Boisselle <romain@boisselle.dev>
2021-02-26 15:00:04 +0100

    defaults values does not work?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -l
 # inspired by https://gist.github.com/romainbsl/c60987c4852f9e56e3b0ebc51ee2b7c9
 
+echo "$INPUT_BASE_URL"
+if [ -z $INPUT_BASE_URL ]
+then
+  INPUT_BASE_URL="https://oss.sonatype.org/service/local/"
+fi
+
 response=$(curl -s --request POST -u "$INPUT_USERNAME:$INPUT_PASSWORD" \
   --url ${INPUT_BASE_URL}staging/bulk/drop \
   --header 'Accept: application/json' \
